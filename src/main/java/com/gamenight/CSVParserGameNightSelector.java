b88d2b659551bd3b3765ea13d4afd354dbbcf7cf
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-06-27 13:05:46 -0700

    Added game.csv & parsing for games.

--- src/main/java/com/gamenight/CSVParserGameNightSelector.java
+++ src/main/java/com/gamenight/CSVParserGameNightSelector.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class CSVParserGameNightSelector {
-    public static void main(String[] args) {
+    public static void getPlayerInfo() {
 
         String path = "src/main/resources/PlayerInfo.csv";
 
@@ -21,7 +21,33 @@ public class CSVParserGameNightSelector {
                     String age = row.get(1);
                     String gameType = row.get(2);
                     System.out.println("Name: " + name + ", Age: " + age + ", GameType: " + gameType);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //public static void getGameInfo() {
+    public static void main(String[] args) {
 
+        String path = "src/main/resources/Games.csv";
+
+        try (Reader in = new FileReader(path)) {
+            for (CSVRecord row : CSVFormat.EXCEL.parse(in)) {
+                if (row.get(0).equals("playerName")) {
+                    continue;
+                }
+                String playerName = row.get(0);
+                String gameName = row.get(1);
+                String gameType = row.get(2);
+                String minPlayerCount = row.get(3);
+                String maxPlayerCount = row.get(4);
+                String minPlayerAge = row.get(5);
+                String minGameTime = row.get(6);
+
+                System.out.println("Name: " + playerName + ", Game: " + gameName +
+                        ", GameType: " + gameType + ", Min # Players: " + minPlayerCount
+                        + ", Max # Players: " + maxPlayerCount + ", Min Player Age: " + minPlayerAge
+                        + ", Min Game Time: " + minGameTime);
             }
         } catch (IOException e) {
             e.printStackTrace();
