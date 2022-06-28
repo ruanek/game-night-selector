dd3b28f6d7446df08a8c2239e947d4bf013e4d3b
KitaGaither <lakitgai@amazon.com>
2022-06-27 23:16:10 -0700

    Added Java Swing GUI

---
+++ src/main/java/com/gamenight/GUI.java
@@ -0,0 +1,46 @@
+package com.gamenight;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI() {
+
+        frame = new JFrame();
+        label = new JLabel("Tonight's Game");
+        JButton button = new JButton("Choose Players");
+        button.addActionListener(this);
+        button.setBackground(Color.PINK);
+        button.setForeground(Color.BLACK);
+
+
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setTitle("Game Night Selector");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
