763a2f63824d3f51310bf4b61a87cb8b65950bce
KitaGaither <lakitgai@amazon.com>
2022-06-28 00:42:05 -0700

    Added player names and checkboxes

--- src/main/java/com/gamenight/GUI.java
+++ src/main/java/com/gamenight/GUI.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class GUI implements ActionListener {
 
@@ -12,10 +13,43 @@ public class GUI implements ActionListener {
     private JPanel panel;
 
     public GUI() {
-
         frame = new JFrame();
-        label = new JLabel("Tonight's Game");
-        JButton button = new JButton("Choose Players");
+        label = new JLabel("Choose Players");
+
+        JCheckBox billButton = new JCheckBox("Bill");
+        billButton.setMnemonic(KeyEvent.VK_C);
+        billButton.setSelected(true);
+
+        JCheckBox bobButton = new JCheckBox("Bob");
+        bobButton.setMnemonic(KeyEvent.VK_C);
+        bobButton.setSelected(true);
+
+        JCheckBox carrieButton = new JCheckBox("Carrie");
+        carrieButton.setMnemonic(KeyEvent.VK_C);
+        carrieButton.setSelected(true);
+
+        JCheckBox janeButton = new JCheckBox("Jane");
+        janeButton.setMnemonic(KeyEvent.VK_C);
+        janeButton.setSelected(true);
+
+        JCheckBox joeButton = new JCheckBox("Joe");
+        joeButton.setMnemonic(KeyEvent.VK_C);
+        joeButton.setSelected(true);
+
+        JCheckBox josephButton = new JCheckBox("Joseph");
+        josephButton.setMnemonic(KeyEvent.VK_C);
+        josephButton.setSelected(true);
+
+        JCheckBox robButton = new JCheckBox("Rob");
+        robButton.setMnemonic(KeyEvent.VK_C);
+        robButton.setSelected(true);
+
+        JCheckBox timButton = new JCheckBox("Tim");
+        timButton.setMnemonic(KeyEvent.VK_C);
+        timButton.setSelected(true);
+
+
+        JButton button = new JButton("Choose Game");
         button.addActionListener(this);
         button.setBackground(Color.PINK);
         button.setForeground(Color.BLACK);
@@ -25,8 +59,16 @@ public class GUI implements ActionListener {
         panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
         panel.add(label);
+        panel.add(billButton);
+        panel.add(bobButton);
+        panel.add(carrieButton);
+        panel.add(janeButton);
+        panel.add(joeButton);
+        panel.add(josephButton);
+        panel.add(robButton);
+        panel.add(timButton);
+        panel.add(button);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setTitle("Game Night Selector");
