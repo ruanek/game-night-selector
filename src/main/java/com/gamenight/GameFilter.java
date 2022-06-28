393df299bc98f0b94de205250d3bf0dda561cd9f
“Shaquille <shaqmandy@gmail.com>
2022-06-28 11:09:54 -0700

    created helper methods for the filterGames method

--- src/main/java/com/gamenight/GameFilter.java
+++ src/main/java/com/gamenight/GameFilter.java
@@ -1,41 +1,57 @@
 package com.gamenight;
 
-import com.gamenight.BoardGame;
+import com.sun.security.jgss.GSSUtil;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class GameFilter {
     // TODO call getPlayerInfo > figure out how selected players are being pushed
-    static ArrayList<PlayerInfo> playerInfoArray = CSVParserGameNightSelector.getPlayerInfo();
+    private static ArrayList<PlayerInfo> playerInfoArray = CSVParserGameNightSelector.getPlayerInfo();
     // TODO call getGameInfo > figure out how selected games are being filtered
-    Set<BoardGame> games = CSVParserGameNightSelector.getGameInfo();
+    private static Set<BoardGame> games = CSVParserGameNightSelector.getGameInfo();
+    private static ArrayList<PlayerInfo> playersInAttendance;
     public static int getPlayerCount() {
-        return playersInAttendance().size();
+        return playersInAttendance.size();
     }
 
-    public static ArrayList<PlayerInfo> playersInAttendance() {
-        System.out.println("List the players that will be in attendance, separate each player by comma's : ");
-        System.out.println("Example: Bill,Bob,Larry");
-        Scanner scanner = new Scanner(System.in);
-        String[] playersInAttendance = scanner.nextLine().split(",");
-        ArrayList<PlayerInfo> playersInAttendanceArrayList = new ArrayList<>();
+    public static ArrayList<PlayerInfo> getPlayersInAttendance() {
+        if (playersInAttendance == null) {
+            System.out.println("List the players that will be in attendance, separate each player by comma's : ");
+            System.out.println("Example: Bill,Bob,Larry");
+            Scanner scanner = new Scanner(System.in);
+            String[] playersInAttendanceInput = scanner.nextLine().split(",");
+            playersInAttendance = new ArrayList<>();
 
-        for(String currentPlayer : playersInAttendance) {
-            for(PlayerInfo player : playerInfoArray) {
-                if(player.getPlayerName().equals(currentPlayer)) {
-                    playersInAttendanceArrayList.add(player);
+            for(String currentPlayer : playersInAttendanceInput) {
+                for(PlayerInfo player : playerInfoArray) {
+                    if(player.getPlayerName().equals(currentPlayer)) {
+                        playersInAttendance.add(player);
+                    }
                 }
             }
         }
-        return playersInAttendanceArrayList;
+        return playersInAttendance;
     }
 
 
 
     public static int getAgeRestriction() {
-        return 0;
+        int minAge = Integer.MAX_VALUE;
+        for(PlayerInfo player : playersInAttendance) {
+            minAge = Math.min(player.getPlayerAge(), minAge);
+        }
+        return minAge;
+    }
+
+    public static Set<GameType> gameTypePreference() {
+        Set<GameType> gameTypes = new HashSet<>();
+        for(PlayerInfo player : playersInAttendance) {
+            gameTypes.add(player.getGameType());
+        }
+        return gameTypes;
     }
 
 
@@ -59,6 +75,19 @@ public class GameFilter {
     }
 
     public static void main(String[] args) {
-        int length = getPlayerCount();
+        getPlayersInAttendance();
+
+        int playerCount = getPlayerCount();
+        System.out.printf("Player Count is: %d", playerCount);
+        System.out.println();
+
+        int age = getAgeRestriction();
+        System.out.printf("Age restriction is: %d", age);
+        System.out.println();
+
+        Set<GameType> gameType = gameTypePreference();
+        System.out.println("These are the following Game Types to choose from base on Players in attendance");
+        gameType.forEach((g) -> System.out.println(g));
+        System.out.println();
     }
 }
