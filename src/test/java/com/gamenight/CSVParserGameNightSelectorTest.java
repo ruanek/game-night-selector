ed3f13cf2517e7ed7db61746e677fd1f607ecb4f
“Shaquille <shaqmandy@gmail.com>
2022-06-29 09:43:52 -0700

    Implemented testing for the player and game class, removed header minGameTime in csv

--- src/test/java/com/gamenight/CSVParserGameNightSelectorTest.java
+++ src/test/java/com/gamenight/CSVParserGameNightSelectorTest.java
@@ -8,9 +8,25 @@ class CSVParserGameNightSelectorTest {
 
     @Test
     void getPlayerInfo() {
+        PlayerInfo player = new PlayerInfo("Bill", 23, GameType.RESOURCE_CONTROL);
+
+        // test the Obj setters and ctor to make sure the object came out correct
+        assertEquals("Bill", player.getPlayerName());
+        assertEquals(23, player.getPlayerAge());
+        assertEquals(GameType.RESOURCE_CONTROL, player.getGameType());
     }
 
     @Test
     void getGameInfo() {
+        BoardGame game = new BoardGame("Bill", "Dune",
+                GameType.RESOURCE_CONTROL, 2, 6, 14);
+
+        // test the Obj setters and ctor to make sure the object came out correct
+        assertEquals("Dune", game.getGameName());
+        assertEquals("Bill", game.getPlayerName());
+        assertEquals(GameType.RESOURCE_CONTROL, game.getType());
+        assertEquals(2, game.getMinPlayerCount());
+        assertEquals(6, game.getMaxPlayerCount());
+        assertEquals(14, game.getMinPlayerAge());
     }
 }
